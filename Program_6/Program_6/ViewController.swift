8e81f963dea94fdfabe079090431f20b6c979c2c
Kevin Ren <kevin@Kevins-MacBook-Pro.local>
2019-04-25 01:24:11 -0400

    added onclick functions

--- Program_6/Program_6/ViewController.swift
+++ Program_6/Program_6/ViewController.swift
@@ -15,6 +15,40 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    -(IBAction)signUpUserPressed:(id)sender
+    {
+    //1
+    PFUser *user = [PFUser user];
+    //2
+    user.username = self.userRegisterTextField.text;
+    user.password = self.passwordRegisterTextField.text;
+    //3
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+    if (!error) {
+    //The registration was successful, go to the wall
+    [self performSegueWithIdentifier:@"SignupSuccesful" sender:self];
+    
+    } else {
+    //Something bad has occurred
+    NSString *errorString = [[error userInfo] objectForKey:@"error"];
+    UIAlertView *errorAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:errorString delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+    [errorAlertView show];
+    }
+    }];
+    }
+    
+    -(IBAction)signUpUserPressed:(id)sender
+    {
+    //TODO
+    //If signup sucessful:
+    //[self performSegueWithIdentifier:@"SignupSuccesful" sender:self];
+    }
+    
+    -(IBAction)logInPressed:(id)sender
+    {
+    //If user logged succesful:
+    //[self performSegueWithIdentifier:@"LoginSuccesful" sender:self];
+    
+    }
 }
 
